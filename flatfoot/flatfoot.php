df7b4262d4aaeab6b107f0482822aacfd69809cd
Nick Schlobohm <nickyschlobs@gmail.com>
2015-06-13 12:41:19 +1000

    Moved from Eclipse to Visual Studio

---
+++ flatfoot/flatfoot.php
@@ -0,0 +1,10 @@
+<?php
+if (! defined('indirect')) {
+    die('Direct access prohibited.');
+}
+
+class flatfoot
+{
+
+    const VERSION = "1.1 MINIMAL";
+}
---
+++ flatfoot/vendor/sty.header.php
@@ -0,0 +1,16 @@
+<?php
+if (! defined('indirect')) {
+    die('Direct access prohibited.');
+}
+require_once ((dirname(__FILE__)) . '/../flatfoot.php');
+?>
+<!-- flatfoot <?php echo (flatfoot::VERSION); ?> by Nick Schlobohm -->
+<!DOCTYPE html>
+<html>
+<head>
+<title>flatfoot</title>
+<link href="sty/bootstrap.min.css" rel="stylesheet">
+<link href="sty/custom.css" rel="stylesheet">
+<link href="sty/font-awesome.min.css" rel="stylesheet">
+</head>
+<body>
